137ddb0469a52a076e0faafc1c27efacf3f0a184
lucas-delazeri <lucasdelazeris@gmail.com>
2026-03-28 01:58:05 -0300

    fix: add env on gitignore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,18 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpQmzcd1MzkwGAEyIRMT3bh0a96ezKKYU",
-  authDomain: "mini-blog-222c5.firebaseapp.com",
-  projectId: "mini-blog-222c5",
-  storageBucket: "mini-blog-222c5.firebasestorage.app",
-  messagingSenderId: "432814156853",
-  appId: "1:432814156853:web:6f34032a7efb57ba17d57c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
-
 const auth = getAuth(app);
 
 export { db, auth };
